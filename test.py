d4bcabe4c1d061d8c1aefdcf7ae2a0c7b1a85ba7
Gilles RIVIERE <griviere07@gmail.com>
2015-05-21 10:10:32 +0200

    ajout des tests

---
+++ test.py
@@ -0,0 +1,29 @@
+import tennis
+import unittest
+
+class TennisTest(unittest.TestCase):
+
+	def test_peut_marquer_un_point(self):
+		Match = Tennis()
+		federer = Joueur("federer")
+		nadal = Joueur("Nadal")
+
+		Match.MarquerPoint(federer)
+
+		self.assertEqual(Match.PointsDuJoueur(federer), 15);
+		return 0
+
+	def test_peut_provoquer_un_tie_break(self):
+		return 0
+
+	def test_peut_gagner_un_jeu(self):
+		return 0
+
+	def test_peut_gagner_un_set(self):
+		return 0
+
+	def test_peut_gagner_un_match(self):
+		return 0
+
+
+	
