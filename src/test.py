192cbdff92dd3817abc7ed65c4ef08e9ce0ca623
Gilles RIVIERE <griviere07@gmail.com>
2015-05-21 10:44:02 +0200

    fixed typo in oimport

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-import tennis
+import Tennis from tennis
 import unittest
 
 class TennisTest(unittest.TestCase):
@@ -11,14 +11,15 @@ class TennisTest(unittest.TestCase):
 		Match.MarquerPoint(federer)
 
 		self.assertEqual(Match.PointsDuJoueur(federer), "15");
-		return 0
-
-	def test_peut_provoquer_un_tie_break(self):
-		return 0
 
 	def test_peut_gagner_un_jeu(self):
 		return 0
 
+
+	def test_peut_provoquer_un_tie_break(self):
+		return 0
+
+	
 	def test_peut_gagner_un_set(self):
 		return 0
 
